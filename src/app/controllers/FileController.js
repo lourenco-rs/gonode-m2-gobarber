24a9ee2d01550586e348bc3765a167f8790a9c98
Rodrigo Lourenço <lourenco.rs@gmail.com>
2018-12-12 18:29:47 -0200

    Aula 18 - Model & Migration Agendamento

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+class FileController {
+  show (req, res) {
+    const { file } = req.params
+
+    const filePath = path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads', file)
+
+    console.log('filePath: ', filePath)
+
+    return res.sendFile(filePath)
+  }
+}
+
+module.exports = new FileController()
